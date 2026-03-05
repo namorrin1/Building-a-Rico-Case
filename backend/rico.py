5e541c301b2ce2e257982da666faf006b1b0eb9c
namorrin1 <nicholas.morrin@marquette.edu>
2026-03-04 20:09:13 -0600

    added input simulation for demonstration and polished

--- backend/rico.py
+++ backend/rico.py
@@ -1,33 +1,133 @@
 from lib.BirdBrain import Finch
 #import keyboard
+import threading 
 import time
 finch = Finch('A')
 #finch.setMove('F', 10, 100)
 #finch.setTurn('R', 360, 30)
-def main(): 
-    roomba()
-    '''
-    count = 0
+def main():
+    print("Hello. Welcome to the Rico Case: \n")
+    #roomba()
+    #drawMode('hexagon', 10)
+    #roomba('go')
+    coreLoop()
+
+    
+    
+
+def coreLoop():
     toggle = True
     while(toggle):
-        distance = finch.getDistance()
-        print("distance:",distance)
-        if (count > 20):
-            toggle = False
-        count+=1
-    ''' 
+        choice = input(
+        "Press 'A' to activate Manual Control Mode \n" \
+        "Press 'B' to activate Write Mode \n" \
+        "Press 'C' to activate Roomba Mode \n" \
+        "Press 'D' to activate Draw Mode \n" \
+        "Press 'E' to activate Song Mode \n" \
+        "Press 'Q' to quit\n")
+        toggle = switch(choice.upper())
 
-    '''
-    distance = finch.getDistance()
-    print("distance: ", distance)
-    for i in range (1, 10):
-        print("distance: ", distance)
-        distance = finch.getDistance() 
-    '''
+def switch(choice):
+    match choice: 
+        case 'A':
+            print("Welcome to Manual Control Mode \n")
+            #manual code goes here
+            return True
+        case 'B':
+            print("Welcome to Write Mode \n")
+            usrStrInput = input("Enter the phrase you want Rico to repeat: \n"
+                           "The phrase cannot exceed 15 characters \n" \
+                           "Enter phrase here:")
+            write(usrStrInput)
+            return True
+        case 'C':
+            print("Welcome to Roomba Mode \n")
+            usrRoombaToggle = input("Enter 'Go' to make Rico become a Roomba \n"                 
+                                    "Enter command here: ")
+            if usrRoombaToggle.upper() == 'GO':
+                roomba()
+            
+            return True
+        case 'D': 
+            print("Welcome to Draw Mode \n")
+            usrDrawInput = input("Choose a shape for Rico to draw: \n" \
+                                "Type 'Square' for Rico to draw a Square \n" \
+                                "Type 'Hexagon' for Rico to draw a Hexagon \n" \
+                                "Type 'Star' for Rico to draw a Star \n" \
+                                "Enter your choice here: ")
+            usrDrawLength = int(input("Enter the length for your Shape: "))
+            drawMode(usrDrawInput, usrDrawLength)
+            return True
+        case 'E':
+            print("Welcome to Song Mode \n")
+            usrSongChoice = input("Choose a song for Rico to sing: \n" \
+                                "Type 'Mary' for Mary Had a Little Lamb \n" \
+                                "Type 'Saints' for Oh When the Saints Come Marching In \n" \
+                                "Type 'Twinkle' for Twinkle Twinkle Little Star \n" \
+                                "Enter your choice here: ")
+            
+            songMode(usrSongChoice)
+
+            return True
+        case 'Q':
+            return False
+        case _:
+            print("Not a valid choice! Please Enter a Letter from A -> E")
+            return True
+        
+def drawMode(drawInput, drawLength):
+    if drawInput.upper() == 'SQUARE':
+        makeSquare(drawLength)
+    elif drawInput.upper() == 'HEXAGON':
+        makeHex(drawLength)
+    elif drawInput.upper() == 'STAR':
+        makeStar(drawLength)
+
+def songMode(songChoice):
+    if songChoice.upper() == 'MARY':
+        makeMary()
+    elif songChoice.upper() == 'SAINTS':
+        makeSaints()
+    elif songChoice.upper() == 'TWINKLE':
+        makeTwinkle()
+
 
 def roomba():
-    roombaToggle = True
+    status = {'active' : True}
+    def drive():
+        #this runs in the background thread 
+        while status['active']:
+            distance = finch.getDistance()
+            if distance > 15: 
+                finch.setMotors( 15, 15)
+            else:
+                finch.stop() 
+                finch.setTurn('L', 90, 50)
+            time.sleep(0.05) #this was recommended so the loop doesn't overwork the CPU
+        finch.stop()
+    #background thread
+    mover = threading.Thread(target=drive)
+    mover.daemon = True
+    mover.start()
+
+    while True: 
+        stopCommand = input("Type 'STOP' to end roomba mode: \n").upper()
+        if stopCommand == 'STOP':
+            status['active'] = False
+            break
+    mover.join()
+    print("Roomba Mode Ended")
+'''
+def roomba(userRoombaToggle):
+    if userRoombaToggle.upper() == 'GO':
+        roombaToggle = True
+    elif userRoombaToggle.upper() == 'STOP':
+        roombaToggle = False
+    print("Enter command here:")
     while(roombaToggle):
+        innerRoombaInput = input()
+        if innerRoombaInput.upper() == 'STOP':
+            roombaToggle = False
         distance = finch.getDistance()
         print("distance: ", distance)
         while(distance > 15):
@@ -36,13 +136,19 @@ def roomba():
             print("distance: ", distance)
         finch.setTurn('L', 90, 75)
         distance = finch.getDistance()
+        
             #finch.setTurn('L', 90, 75)
        
             #finch.setMove('F', 50, 50)
-
-def write(str):
+'''
+def write(userStr):
     #str = "Hello World"
-    finch.print(str)
+    if (len(userStr) > 15):
+        finch.print("Too Long!")
+    elif (len(userStr)< 1):
+        finch.print("Too Short!") #wont ever happen 
+    else:
+        finch.print(userStr)
 def makeSquare(length):
     i = 0
     while(i <4):
