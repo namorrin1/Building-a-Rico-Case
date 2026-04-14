7e45dbe6ac375ac4c2e859b0e682e02d77c2c79e
Farzeen Harunani <farzeen@outlook.com>
2026-04-14 16:47:26 -0500

    switched from jest to vitest

--- frontend/src/__tests__/sum.test.js
+++ frontend/src/__tests__/sum.test.js
@@ -2,7 +2,8 @@
 // in the frontend folder, you should be able to run 'npm test' to verify.
 // a 'coverage' folder will be created in the 'frontend' folder that shows test results.
 
-const sum = require('../sum');
+import sum from '../sum'
+import { expect, test } from 'vitest'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
