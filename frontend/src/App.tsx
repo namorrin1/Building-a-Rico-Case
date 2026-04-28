c994cc5465a99a6dd3be8389a0766f2b7a1c9567
Nicholas Morrin <nicholas.morrin@marquette.edu>
2026-04-28 05:12:40 -0500

    I need to sleep

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,9 +13,12 @@ const socket = io(URL, {
       path: '/socket.io/',
       transports: ['websocket'],
     })
+
 function App() {
     const [connected, setConnected] = useState(false)
     const [on , setOn] = useState(false)
+    const [drawSize, setDrawSize] = useState(100)
+    const [message, setMessage] = useState('')
     useEffect(() => {
 
     function onConnect() {
@@ -75,51 +78,181 @@ function App() {
             { connected ? 'connected' : 'not connected' }
             </div>
         </div>
-        <div> 
+       {/* wrapping buttons in this flex div */}
+        <div style ={{ display: 'flex', gap: '16px', padding: PADDING, justifyContent: 'center' }}> 
             <Button
+                variant ='outlined'
+                sx ={{
+                    backgroundColor: '#7ce4be',
+                    color: '#ffffff',
+                    '&:hover': {backgroundColor: '#3ada9f', border: '1px solid black' },
+                    width: '150px',
+                    height: '50px',
+                    fontSize: '16px',
+                    border: '1px solid #3ada9f',
+                    
+                
+                }}
                 onClick={() => {
                     if (on) {
                         socket.emit('move', {direction: 'stop'})
                     }
                     setOn(!on)
-                }}>
+                }}
+            >
                 Move
             </Button>
-        </div>
-        <div id="roombabutton"> 
+        
             <Button
+                sx={{
+                    backgroundColor: '#7ce4be',
+                    color: '#ffffff',
+                    '&:hover': {backgroundColor: '#3ada9f' },
+                    width: '150px',
+                    height: '50px',
+                    fontSize: '16px',
+                }}
                 onClick={() => 
                     socket.emit('roomba')
                 }>
-                Roomba</Button>
+                Roomba
+            </Button>
         </div>
-        <div id="songbuttons">
+        {/*wrapping songbuttons like the other two */}
+        <div id="songbuttons" style={{display: 'flex', gap: '16px', padding: PADDING, justifyContent:'center'}}>
             <Button
+                sx={{
+                    backgroundColor: '#7ce4be',
+                    color: 'white',
+                    '&:hover': {backgroundColor: '#3ada9f' },
+                    width: '180px',
+                    height: '70px',
+                    fontSize: '12px',
+                }}
                 onClick={() => 
                     socket.emit('songhandler','mary')
                 }
             >Mary Had A Little Lamb</Button>
             <Button 
+                sx={{
+                    backgroundColor: '#7ce4be',
+                    color: '#ffffff',
+                    '&:hover': {backgroundColor: '#3ada9f' },
+                    width: '180px',
+                    height: '70px',
+                    fontSize: '12px',
+                }}
                 onClick={() => 
                     socket.emit('songhandler','twinkle')
                 }>Twinkle Twinkle Little Star</Button>
             <Button
+            sx={{
+                    backgroundColor: '#7ce4be',
+                    color: '#ffffff',
+                    '&:hover': {backgroundColor: '#3ada9f' },
+                    width: '180px',
+                    height: '70px',
+                    fontSize: '12px',
+                }}
                 onClick={() => 
                     socket.emit('songhandler','saints')
                 }
             >Oh When The Saints Come Marching In</Button> 
         </div>
-        <div id="message"> 
-                <Box
-      component="form"
-      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-    </Box>
+        <div style= {{display: 'flex', gap:'16px',padding:PADDING, justifyContent: 'center'}}>
+            <TextField
+                label="Size"
+                type="Number"
+                value={drawSize}
+                onChange={(e) => setDrawSize(Number(e.target.value))}
+                sx={{width:'80px', 
+                    
+                    '& .MuiOutlinedInput-root': {
+                        backgroundColor: '#7ce4be',
+                        height: '50px',
+                        alignItems: 'center',
+                    },
+                    '& .MuiInputLabel-root': {
+                        color: '#ffffff',
+                    },
+                    '& .MuiOutlinedInput-input': {
+                        color: '#ffffff',
+                    },
+                    '& .MuiOutlinedInput-notchedOutline': {
+                        border: '1px solid #3ada9f'
+                    },
+                    '&:hover .MuiOutlinedInput-notchedOutline': {
+                        border: '1px solid black'
+                    },
+
+                }}
+            />
+            <Button
+                sx={{
+                    backgroundColor: '#7ce4be',
+                    color:'#ffffff',
+                    '&:hover': {backgroundColor: '#3ada9f'},
+                    width: '120px',
+                    height: '50px',
+                    fontSize: '12px',
+                
+                }}
+                onClick={() => socket.emit('draw', drawSize, 'SQUARE')}
+            >Square</Button>
+
+            <Button
+                sx={{
+                    backgroundColor: '#7ce4be',
+                    color:'#ffffff',
+                    '&:hover': {backgroundColor: '#3ada9f'},
+                    width: '120px',
+                    height: '50px',
+                    fontSize: '12px',
+                
+                }}
+                onClick={() => socket.emit('draw', drawSize, 'HEXAGON')}
+            >Hexagon</Button>
+
+            <Button
+                sx={{
+                    backgroundColor: '#7ce4be',
+                    color:'#ffffff',
+                    '&:hover': {backgroundColor: '#3ada9f'},
+                    width: '120px',
+                    height: '50px',
+                    fontSize: '12px',
+                
+                }}
+                onClick={() => socket.emit('draw', drawSize, 'STAR')}
+            >Star</Button>
+        </div>
+        <div style={{display: 'flex', gap: '16px', padding: PADDING, justifyContent: 'center', alignItems: 'center'}}>
+            <TextField 
+                label="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                sx={{
+                    width: '180px',
+                    '& .MuiOutlinedInput-root': {backgroundColor: '#7ce4be', height: '70px'},
+                    '& .MuiInputLabel-root': {color: '#ffffff'},
+                    '& .MuiOutlinedInput-input': {color: '#ffffff'},
+
+                }}
+            />
+            <Button
+                sx={{
+                    backgroundColor: '#7ce4be',
+                    color: '#ffffff',
+                    '&:hover':{ backgroundColor: '#3ada9f'},
+                    width:'180px',
+                    height: '70px',
+                    fontSize: '14px',
+
+                }}
+                onClick={() => socket.emit('write', message)}
+            >
+                Print Message
+            </Button>
         </div>
         </>
     )
